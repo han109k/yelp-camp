fe5ee26f8200d1cd8dbdac310524de9f63267b04
mkx <han109k@gmail.com>
2021-05-24 23:47:43 +0300

    add npm start

--- app.js
+++ app.js
@@ -208,6 +208,9 @@ app.use((err, req, res, next) => {
     res.status(status).render("error", { err })
 })
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000.");
+// on heroku port will usually be 80
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}.`);
 })
